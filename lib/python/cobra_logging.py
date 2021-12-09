78d6f2f60477cf2b51f61529bb33f1f344ecbe93
Marco Bradtke <marco.bradtke@ge.com>
2021-12-09 23:38:57 +0100

    Added python logging to elastic

--- lib/python/cobra_logging.py
+++ lib/python/cobra_logging.py
@@ -1,7 +1,9 @@
 import sys
 import os
 import logging
-
+import requests
+import datetime
+import socket
 class Logger(object):
     '''
     Logging class for Python
@@ -20,19 +22,56 @@ class Logger(object):
             self.classname = parent_class
 
         self.level = os.environ["LOG_LEVEL"]
+
+    def _get_timestamp_(self):
+
+        return str(datetime.datetime.utcnow().isoformat())
+
+    def _get_base_body_(self):
+
+        log_body = {
+            '@timestamp': self._get_timestamp_(),
+            'pod': socket.gethostname(),
+            'class': self.classname
+        }
+
+        return log_body
+
+    def _send_log_(self, log_body):
+
+        response = requests.post('http://elastic:9200/python_logs/_doc',json=log_body)
+
+        if response.status_code >= 300:
+            print(f'Could not create log. Status code: {response.status_code}')
             
     def error(self, message, error_object=None):
 
-        logging.error(f'ERROR [{self.classname}] {message}')
-        
+        log_body = self._get_base_body_()
+        log_body['type'] = 'ERROR'
+        log_body['message'] = message
+        log_body['error'] = str(error_object)
+
+        self._send_log_(log_body)
+
 
     def info(self, message):
 
         if self.level == 'INFO' or self.level == 'DEBUG':
-            logging.info(f'INFO [{self.classname}] {message}')
-            
+
+            log_body = self._get_base_body_()
+            log_body['type'] = 'INFO'
+            log_body['message'] = message
+  
+            self._send_log_(log_body)
+
     def debug(self, message):
+
         if self.level == 'DEBUG':
-            logging.debug(f'DEBUG [{self.classname}] {message}')
+
+            log_body = self._get_base_body_()
+            log_body['type'] = 'DEBUG'
+            log_body['message'] = message
+  
+            self._send_log_(log_body)
             
         
