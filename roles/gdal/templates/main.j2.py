bd1228fad64e54b146b03d6fa2554a2e4c5f5590
Marco Bradtke <marco.bradtke@ge.com>
2021-12-12 15:41:51 +0100

    gdal pod picking up gdal jobs

--- roles/gdal/templates/main.j2.py
+++ roles/gdal/templates/main.j2.py
@@ -4,24 +4,29 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 l = cobra_logging.Logger('GDAL Flask')
 l.debug('Start Cobra Logging')
+
+#TODO: Schema handling
+cobra_gdal.cobra_gdal(schema='gis', run_in_loop=True)
+
 @app.route('/')
 def hello_world():
-    l.debug('hello_world')
-    return 'Hello, Docker!'
+    l.debug('hello gdal')
+    return 'Hello, GDAL!'
 
-@app.route('/load_shape', methods=['POST'])
-def load_shape():
-    app.logger.info('load_shape')
-    data = request.json
-    g = cobra_gdal.cobra_gdal()
-    try:
-        app.logger.debug(data)
-        #app.logger.info(f'{data['path']}')
-        g.shape2pg(data.get('path'))
-    except Exception as inst:
-        app.logger.error(inst)
-    finally:
-        return jsonify(data)
+
+#@app.route('/load_shape', methods=['POST'])
+#def load_shape():
+#    app.logger.info('load_shape')
+#    data = request.json
+#    g = cobra_gdal.cobra_gdal()
+#    try:
+#        app.logger.debug(data)
+#        #app.logger.info(f'{data['path']}')
+#        g.shape2pg(data.get('path'))
+#    except Exception as inst:
+#        app.logger.error(inst)
+#    finally:
+#        return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- roles/jobregistry/templates/main.j2.py
+++ roles/jobregistry/templates/main.j2.py
@@ -1,24 +1,42 @@
 import cobra_logging
+import cobra_jobregistry
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 l = cobra_logging.Logger('jobregistry Flask')
-l.debug('Start Cobra Logging')
+
+reg = cobra_jobregistry.cobra_jobregistry()
+
 @app.route('/')
 def hello_world():
     l.debug('hello_world')
     return 'Hello, Docker!'
 
+def data_or_default(key, dict, default_val):
+  
+    if key in dict.keys():
+      return dict[key]
+    else:
+      return default_val
+
 @app.route('/job', methods=['POST'])
 def job():
-    app.logger.info('POST job')
+    l.info('POST job')
     data = request.json
-    g = cobra_gdal.cobra_gdal()
+    
     try:
-        app.logger.debug(data)
-        #app.logger.info(f'{data['path']}')
-        g.shape2pg(data.get('path'))
+        l.debug(data)
+
+        path = data['path']
+        job_name = data_or_default('job_name',data,None)
+        skip_failures = bool(data_or_default('skip_failures',data,True))
+        priority = int(data_or_default('priority',data,42))
+        
+        #TODO: Check/fix skip_failures
+        reg.create_shape2pg_job(path, job_name=job_name, priority=priority, skip_failures=skip_failures)
+        l.info('jobcreated')
     except Exception as inst:
+        l.error("job failed", inst)
         app.logger.error(inst)
     finally:
         return jsonify(data)
